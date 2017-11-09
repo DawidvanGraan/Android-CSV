993f7644b9825c5eef214f34f67d85a0fd2223ae
Dawid van Graan <dawid.van.graan@gmail.com>
2017-11-09 22:04:31 +0200

    Update CSVGenerator.java
    Redo

--- androidcsv/src/main/java/com/abdymalikmulky/androidcsv/CSVGenerator.java
+++ androidcsv/src/main/java/com/abdymalikmulky/androidcsv/CSVGenerator.java
@@ -105,57 +105,32 @@ public class CSVGenerator extends CSVContent {
         String contentStr = "";
         int contentIndex = isHeader ? 0 : 1;
 
-        if(!stringContainsItemFromList(content,exceptionColumn)) {
-            final String[] equalSplit = content.split("=");
-
-            // Getting a crash where index out of bounds, lenght = 1 and index = 1
-            if (contentIndex > equalSplit.length - 1) {
-                contentStr = "<CHECK YOUR MODEL>";
-            } else {
-                contentStr = equalSplit[contentIndex];
-            }
-            //contentStr = equalSplit[contentIndex];
-
-
-            if (isHeader) {
-                contentStr = contentStr.toUpperCase();
-            }
-
-            //if (i > 0 && !contentTable.equals("")) {
-            //    contentTable = contentTable + COMMA;
-            //}
-
-
-            contentTable += appendQuote(contentStr);
-        }
-        
-
-//         String[] commSplit = content.split(COMMA);
-//         for (int i=0;i<commSplit.length;i++){
-//             if(!stringContainsItemFromList(commSplit[i],exceptionColumn)) {
-//                 String[] equalSplit = commSplit[i].split("=");
+        String[] commSplit = content.split(COMMA);
+        for (int i=0;i<commSplit.length;i++){
+            if(!stringContainsItemFromList(commSplit[i],exceptionColumn)) {
+                String[] equalSplit = commSplit[i].split("=");
                 
-//                 // Getting a crash where index out of bounds, lenght = 1 and index = 1
-//                 if (contentIndex > equalSplit.length - 1) {
-//                     contentStr = commSplit[i];
-//                 } else {
-//                     contentStr = equalSplit[contentIndex];
-//                 }
-//                 //contentStr = equalSplit[contentIndex];
+                // Getting a crash where index out of bounds, lenght = 1 and index = 1
+                if (contentIndex > equalSplit.length - 1) {
+                    contentStr = commSplit[i];
+                } else {
+                    contentStr = equalSplit[contentIndex];
+                }
+                //contentStr = equalSplit[contentIndex];
 
 
-//                 if (isHeader) {
-//                     contentStr = contentStr.toUpperCase();
-//                 }
+                if (isHeader) {
+                    contentStr = contentStr.toUpperCase();
+                }
 
-//                 if (i > 0 && !contentTable.equals("")) {
-//                     contentTable = contentTable + COMMA;
-//                 }
+                if (i > 0 && !contentTable.equals("")) {
+                    contentTable = contentTable + COMMA;
+                }
 
 
-//                 contentTable += appendQuote(contentStr);
-//             }
-//         }
+                contentTable += appendQuote(contentStr);
+            }
+        }
 
         return contentTable;
     }
